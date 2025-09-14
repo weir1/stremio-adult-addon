c7198117a59b233539c921070922191a2330f670
Moin <weircarl@gmail.com>
2025-09-14 17:27:49 +0530

    fix all video stream

--- src/routes/addonRoutes.js
+++ src/routes/addonRoutes.js
@@ -12,9 +12,26 @@ const addonMiddleware = (req, res, next) => {
   const configString = req.params.config;
   const userConfig = configString ? ConfigService.parseConfigFromUrl(configString) : {};
   
-  // Create a new handler for each request to get the latest dynamic manifest
   const addon = new AddonHandler(userConfig);
   const sdkRouter = getRouter(addon.getInterface());
+
+  // The SDK router needs a clean URL. We rewrite it here.
+  // e.g., /<config>/manifest.json -> /manifest.json
+  const pathParts = req.path.split('/');
+  const manifestIndex = pathParts.indexOf('manifest.json');
+  const catalogIndex = pathParts.indexOf('catalog');
+  const metaIndex = pathParts.indexOf('meta');
+  const streamIndex = pathParts.indexOf('stream');
+
+  if (manifestIndex > -1) {
+    req.url = '/' + pathParts.slice(manifestIndex).join('/');
+  } else if (catalogIndex > -1) {
+    req.url = '/' + pathParts.slice(catalogIndex).join('/');
+  } else if (metaIndex > -1) {
+    req.url = '/' + pathParts.slice(metaIndex).join('/');
+  } else if (streamIndex > -1) {
+    req.url = '/' + pathParts.slice(streamIndex).join('/');
+  }
   
   sdkRouter(req, res, next);
 };
